b2e773b49b852729444466a9b16cca55f2f7b755
Ahmed Mostafa Eltantawy <ahmedmostafaui@gmail.com>
2021-03-06 19:18:19 +0200

    Added basic database tables migrations

---
+++ database/migrations/2021_03_06_164023_create_taggables_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaggablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->integer('tag_id'); // tag id
+            $table->integer('taggable_id'); // post id / product id
+            $table->string('taggable_type'); // model name ( App\Models\Post ) / ( App\Models\Product )
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');
+    }
+}
