f457a8e2794203082c39d5535e318eff1c5e399b
Ahmed Mostafa Eltantawy <ahmedmostafaui@gmail.com>
2021-03-23 17:02:15 +0200

    Updated api.js

--- app/Traits/Collection/MultTitenanable.php
+++ app/Traits/Collection/MultTitenanable.php
@@ -6,12 +6,10 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait MultTitenanable
 {
-
     public static function bootMultTitenanable()
     {
         static::addGlobalScope('created_by_user', function (Builder $builder) {
             $builder->where('created_by_user', auth()->id());
         });
     }
-
 }
